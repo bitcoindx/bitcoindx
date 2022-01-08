d355d93c447036b266fd074de100c5b29b9e43c1
bitcoindx <miyano.masahiko@protonmail.com>
2022-01-09 03:00:15 +1100

    v22.0.1 Update
    -Rectified halving interval to 105120 blocks.
    -Amended manuals to reflect version details.
    -Updated ICNS file for compiling on macOS.
    -Other cosmetic fixes.

--- src/test/fuzz/fuzz.cpp
+++ src/test/fuzz/fuzz.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2009-2021 The BitcoinDX Core developers
+// Copyright (c) 2021-2022 The BitcoinDX Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
